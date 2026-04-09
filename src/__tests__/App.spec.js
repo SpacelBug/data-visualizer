8d95ad85774f4d1e4c1e780ddc26b8ba6c9d09d5
I love kludge <98ger@mail.ru>
2026-04-09 15:57:06 +1200

    add pinia instance into vitest

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,9 +1,14 @@
-import { describe, it, expect } from 'vitest'
-
+import { describe, beforeEach, it, expect } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
 import { mount } from '@vue/test-utils'
 import App from '../App.vue'
 
 describe('App', () => {
+  beforeEach(() => {
+    // Creates a fresh pinia for every test
+    setActivePinia(createPinia())
+  })
+
   it('mounts renders properly', () => {
     const wrapper = mount(App)
   })
